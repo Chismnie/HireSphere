a88c00300b0834661b80b159e05302f517f59f5d
yanmengs <1657294032@qq.com>
2024-12-31 03:36:42 +0800

    fix: 切换为ts

---
+++ src/view/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "antd";
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Button type="primary">这个是Home切换为ts成功</Button>
+    </div>
+  );
+};
+export default Home;
--- src/view/Login/login.tsx
+++ src/view/Login/login.tsx
@@ -2,7 +2,7 @@ import { Button } from "antd";
 const Login: React.FC = () => {
   return (
     <div>
-      <Button type="primary">这个是登录页</Button>
+      <Button type="primary">这个是登录页切换为ts成功</Button>
     </div>
   );
 };
